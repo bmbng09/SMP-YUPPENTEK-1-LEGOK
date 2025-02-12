be2f8e8b253a796a073b930af416584785194168
Bambang Istijab <bambangistijab30@gmail.com>
2025-02-12 16:28:36 +0700

    Add files via upload

---
+++ sweetalert.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    Swal.fire({
+        // title: 'Brosur Sekolah',
+        imageUrl: 'assets/brosur.png',
+        imageWidth: 600,
+        imageAlt: 'Brosur Sekolah',
+        showCloseButton: true,
+        showConfirmButton: false 
+    });
+};
